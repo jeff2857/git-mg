1ff6fcc98d05b3c59b3c556c78cddf3cf141faaa
Jeff2857 <jeffliu2857@gmail.com>
2022-05-26 17:33:40 +0800

    feat: read commit message

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use std::env;
+
 use anyhow::Result;
 
 use git_mg::git_emergency;
 
 fn main() -> Result<()> {
-    // todo: read input commit message
-    git_emergency("this is a test commit")?;
+    let mut commit_message = String::from("emergency commit!!!");
+
+    let mut args = env::args();
+    args.next();
+
+    match args.next() {
+        Some(message) => {
+            commit_message = message;
+        },
+        None => {},
+    }
+
+    //println!("{}", commit_message);
+    git_emergency(&commit_message)?;
     Ok(())
 }
